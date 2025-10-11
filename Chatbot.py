d80970c99d60cac21125cb617cb107a500e5c838
fernandomarmolejourriago3-cmd <fernando.marmolejo.urriago.3@gmail.com>
2025-10-10 21:25:15 -0500

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -1,7 +1,7 @@
 from nltk.chat.util import Chat, reflections
 
 pares = [
-    [r"hola|hi|hello|Hola Profe", ["¡Hola! ¿Como puedo Ayudarte?, para conocer las opciones de asesoria escribe menu"]],
+    [r"hola|hi|hello|Hola Profe|Hola Profe fernando|Hola Fernando|Fernando|fernando|compañero|Compañero", ["¡Hola! ¿Como puedo Ayudarte?, para conocer las opciones de asesoria escribe "Menú""]],
     [r"Profe|Como estas", ["Bien y tu como estas"]],
     [r"1", ["Opción 1 seleccionada: En el formulario introduce el número de la Fase de la que desea conocer la calificación"]],
     [r"2", ["Opción 2 seleccionada: Agenda con el profe para que te explique la Actividad"]],
@@ -15,3 +15,4 @@ def responder(mensaje):
     if mensaje.lower() in ["menu"]:
         return "Selecciona una opción:\n1️⃣ Calificaciones\n2️⃣ No Comprende la Actividad\n3️⃣ Reclamo de Nota"
     return chat.respond(mensaje) or "No entendí, intenta con otra opción."
+
