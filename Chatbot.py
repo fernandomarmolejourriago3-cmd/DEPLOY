19d9d3374bdc5de4b810a441e5802e809e1aa2e9
fernandomarmolejourriago3-cmd <fernando.marmolejo.urriago.3@gmail.com>
2025-10-10 21:16:45 -0500

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -1,16 +1,17 @@
 from nltk.chat.util import Chat, reflections
 
 pares = [
-    [r"hola|hi|hello", ["¡Hola! ¿Qué deseas hacer?"]],
-    [r"1", ["Opción 1 seleccionada: Información sobre el chatbot"]],
-    [r"2", ["Opción 2 seleccionada: Saludos personalizados"]],
-    [r"3", ["Opción 3 seleccionada: Despedida"]],
-    [r"(.*) adiós", ["¡Hasta luego!"]],
+    [r"hola|hi|hello|Hola Profe", ["¡Hola! ¿Como puedo Ayudarte?, para conocer las opciones de asesoria escribe menu"]],
+    [r"Profe|Como estas", ["Bien y tu como estas"]],
+    [r"1", ["Opción 1 seleccionada: En el formulario introduce el número de la Fase de la que desea conocer la calificación"]],
+    [r"2", ["Opción 2 seleccionada: Agenda con el profe para que te explique la Actividad"]],
+    [r"3", ["Opción 3 seleccionada: Escribe en el formulario campo observación el detalle (Nombre de Evidencia) que desea vaidar nota"]],
+    [r"(.*) adiós|Luego hablamos", ["¡Hasta luego escribeme a este numero de Whatsapp = 3052546933!"]],
 ]
 
 chat = Chat(pares, reflections)
 
 def responder(mensaje):
-    if mensaje.lower() in ["menu", "opciones", "hola"]:
-        return "Selecciona una opción:\n1️⃣ Información\n2️⃣ Saludos\n3️⃣ Despedida"
+    if mensaje.lower() in ["menu"]:
+        return "Selecciona una opción:\n1️⃣ Calificaciones\n2️⃣ No Comprende la Actividad\n3️⃣ Reclamo de Nota"
     return chat.respond(mensaje) or "No entendí, intenta con otra opción."
