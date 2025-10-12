909074a84228d4e935760cb5950be6d359df7713
fernandomarmolejourriago3-cmd <fernando.marmolejo.urriago.3@gmail.com>
2025-10-11 21:56:24 -0500

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -3,7 +3,7 @@ from nltk.chat.util import Chat, reflections
 pares = [
     [r"hola|hi|hello|pola profe|hola profe fernando|hola fernando|fernando|compañero|hoa", ["¡Hola! ¿Como puedo Ayudarte?, para conocer las opciones de asesoria escribe "Menú""]],
     [r"Profe|Como estas", ["Bien y tu como estas"]],
-    [r"1", ["Opción 1 seleccionada: En el formulario (https://forms.office.com/Pages/DesignPageV2.aspx?origin=NeoPortalPage&subpage=design&id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAYAAJpMRvhUQkY3UlBCU0IyTVlORUo4Nkk0NUlTM1k2Ny4u&topview=Prefil) lintroduce el número de la Fase de la que desea conocer la calificación"]],
+    [r"1", ["Opción 1 seleccionada: En el formulario (https://forms.office.com/Pages/DesignPageV2.aspx?origin=NeoPortalPage&subpage=design&id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAYAAJpMRvhUQkY3UlBCU0IyTVlORUo4Nkk0NUlTM1k2Ny4u&topview=Prefil) introduce el número de la Fase de la que desea conocer la calificación"]],
     [r"2", ["Opción 2 seleccionada: Agenda con el profe para que te explique la Actividad"]],
     [r"3", ["Opción 3 seleccionada: Escribe en el formulario campo observación el detalle (Nombre de Evidencia) que desea vaidar nota"]],
     [r"(.*) adiós|Luego hablamos", ["¡Hasta luego escribeme a este numero de Whatsapp = 3052546933!"]],
@@ -16,5 +16,6 @@ def responder(mensaje):
     if mensaje.lower() in ["menu"]:
         return "Selecciona una opción:\n1️⃣ Calificaciones\n2️⃣ No Comprende la Actividad\n3️⃣ Reclamo de Nota"
     return chat.respond(mensaje) or "No entendí, intenta con otra opción."
+
 
 
